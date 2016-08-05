26adb6b40d0404e3e1df27f0d3d52197914d1856
Chris Paynes <cdpaynes@gmail.com>
2016-08-05 18:05:55 -0500

    Update main.js

--- javascript_coursework/ticket_storage/main.js
+++ javascript_coursework/ticket_storage/main.js
@@ -1,13 +1,19 @@
 // foreign key to link ticket data across parallel arrays
 var foreignKey = 0;
 
+// initializes the table by removing all values
+function initTable() {
+  var init_Table_Map = ["foreign_key", "id", "client", "status", "cost"];
+  for(var col in init_Table_Map) {
+    document.getElementById(init_Table_Map[col]).innerHTML = "";
+  }
+}
+
+// clears the records currently displayed in the table
 // reads all production tickets across parallel arrays
 // a foreign key links ticket information across arrays
 function readIndex() {
-<<<<<<< HEAD
   initTable()
-=======
->>>>>>> 8a5acad3ab00e717d4039c5bbec53ffff306ec1c
   document.getElementById("scheduleHeader").innerHTML + idArr.length + " TICKETS";
 
   for (var i in idArr) {
@@ -15,7 +21,7 @@ function readIndex() {
     document.getElementById("id").innerHTML += idArr[i] + "<br>";
     document.getElementById("client").innerHTML += clientArr[i] + "<br>";
     document.getElementById("status").innerHTML += statusArr[i] + "<br>";
-    document.getElementById("cost").innerHTML += "&#36;" + costArr[i] + "<br>";
+    document.getElementById("cost").innerHTML += "&#36;" + costArr[i].toLocaleString() + "<br>";
   }
 };
 
@@ -27,10 +33,10 @@ function getForeignKey(queryId) {
 
 // ensures the id is a six digit integer value
 function validateIdLen(id_length) {
-  if(Number.isInteger(id_length) && id_length.toString().length === 6) {
+  if(Number.isInteger(id_length) && id_length.toString().length === 6 && id_length >= 100000) {
     return true;
   } else {
-    alert("The ticket_id must be a 6 digit integer");
+    alert("The ticket_id must be a positive 6 digit integer");
     return false;
   }
 }
