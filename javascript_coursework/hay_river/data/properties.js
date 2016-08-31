f8f8b6aa1ce6d4ef87e922c12686315231e12a56
chrispaynes <cdpaynes@gmail.com>
2016-08-30 22:51:24 -0500

    Reduces number of variables in favor of functional programming

--- javascript_coursework/hay_river/data/properties.js
+++ javascript_coursework/hay_river/data/properties.js
@@ -1,52 +1,54 @@
-  // var app_maps = [map_1821, map_1818, map_1815, map_N4923];
-  var map_1821;
-  var map_1818;
-  var map_1815;
-  var map_N4923;
+  var map_1821 = {};
+  var map_1818 = {};
+  var map_1815 = {};
+  var map_N4923 = {};
 
 var app_rental_props = [{
   "address": "1821 7th Street",
   "city": "Menomonie",
+  "lat": 44.865782,
+  "lon": -91.922486,
   "state": "WI",
   "zip": "54751",
   "image_src": "1821.jpg",
   "category": "4 Bedroom Apartment",
-  "desc": "DESCRIPTION #1:, 4 Bedrooms, Eat-in kitchen, 2 Bathrooms, Plenty of sun, Deck, Storage, Washing Machine/Dryer, Gas Stove, Personal Butler, Oversized Garage, Option for fully-furnished",
+  "desc": "PROP #1: 4 Bedrooms, Eat-in kitchen, 2 Bathrooms, Plenty of sun, Deck, Storage, Washing Machine/Dryer, Gas Stove, Personal Butler, Oversized Garage, Option for fully-furnished",
   "floor": "floor.jpg",
   "map": map_1821
-  // "map": '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2827.828699678548!2d-91.92467468446247!3d44.86578197909851!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87f87b711939f29f%3A0xa8ffb9dd5d6c1535!2s1821+7th+St+E%2C+Menomonie%2C+WI+54751!5e0!3m2!1sen!2sus!4v1472339747768" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>'
 }, {
   "address": "1818 6th Street",
   "city": "Menomonie",
+  "lat": 44.865979,
+  "lon": -91.9252027,
   "state": "WI",
   "zip": "54751",
   "image_src": "1818.jpg",
   "category": "4 Bedroom Apartment",
-  "desc": "DESCRIPTION #2:, 4 Bedrooms, Eat-in kitchen, 2 Bathrooms, Plenty of sun, Deck, Storage, Washing Machine/Dryer, Gas Stove, Personal Butler, Oversized Garage, Option for fully-furnished",
+  "desc": "PROP #2: 4 Bedrooms, Eat-in kitchen, 2 Bathrooms, Plenty of sun, Deck, Storage, Washing Machine/Dryer, Gas Stove, Personal Butler, Oversized Garage, Option for fully-furnished",
   "floor": "floor.jpg",
   "map": map_1818
-  // "map": '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2827.8188354136955!2d-91.92520268480027!3d44.86598278118066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87f87b713df7f04f%3A0x739712f61997ce4b!2s1818+6th+St+E%2C+Menomonie%2C+WI+54751!5e0!3m2!1sen!2sus!4v1472339805873" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>'
 }, {
   "address": "1815 7th Street",
   "city": "Menomonie",
+  "lat": 44.866102,
+  "lon": -91.9246957,
   "state": "WI",
   "zip": "54751",
   "image_src": "1815.jpg",
   "category": "4 Bedroom Apartment",
-  "desc": "DESCRIPTION #3:, 4 Bedrooms, Eat-in kitchen, 2 Bathrooms, Plenty of sun, Deck, Storage, Washing Machine/Dryer, Gas Stove, Personal Butler, Oversized Garage, Option for fully-furnished",
+  "desc": "PROP #3: 4 Bedrooms, Eat-in kitchen, 2 Bathrooms, Plenty of sun, Deck, Storage, Washing Machine/Dryer, Gas Stove, Personal Butler, Oversized Garage, Option for fully-furnished",
   "floor": "floor.jpg",
   "map": map_1815
-  // "map": '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2827.812793106118!2d-91.9246956848003!3d44.86610578117268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87f87b7122371b97%3A0x9f457850865fac61!2s1815+7th+St+E%2C+Menomonie%2C+WI+54751!5e0!3m2!1sen!2sus!4v1472339859174" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>'
 }, {
   "address": "N4923 - 572nd Street",
   "city": "Menomonie",
+  "lat": 44.8589455,
+  "lon": -91.8753319,
   "state": "WI",
   "zip": "54751",
   "image_src": "N4923.jpg",
   "category": "3 Bedroom Townhouse",
-  "desc": "DESCRIPTION #4:, 4 Bedrooms, Eat-in kitchen, 2 Bathrooms, Plenty of sun, Deck, Storage, Washing Machine/Dryer, Gas Stove, Personal Butler, Oversized Garage, Option for fully-furnished",
+  "desc": "PROP #4: 4 Bedrooms, Eat-in kitchen, 2 Bathrooms, Plenty of sun, Deck, Storage, Washing Machine/Dryer, Gas Stove, Personal Butler, Oversized Garage, Option for fully-furnished",
   "floor": "floor.jpg",
     "map": map_N4923
-
-  // "map": '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2828.164517339864!2d-91.87533188446271!3d44.858945479098445!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87f87ce22c812257%3A0x5dc12a052c7e6ba1!2s572nd+St%2C+Menomonie%2C+WI+54751!5e0!3m2!1sen!2sus!4v1472339948470" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>'
 }];
--- javascript_coursework/hay_river/js/maps.js
+++ javascript_coursework/hay_river/js/maps.js
@@ -1,145 +1,87 @@
 function initMaps(property_id) {
-  // setMapOptions() defines map options for a zoom level, map type, control type
+  // setMapOptions() defines and returns an object of map options
+  // for zoom level, map type, control type
   // and latitude and longitude to center the map app
   // setMapOptions(z *int, mType *string, cStyle *string, lt *float, ln *float)
-  function setMapOptions(z, mType, cStyle, lt, ln){
+  function setMapOptions(lt, ln){
     return  {
               // ZOOM LEVELS: 1: World, 5: Landmass/continent, 10: City, 15: Streets, 20: Buildings
-              zoom: z,
+              zoom: 15,
 
               // mapTypeId accepts *string to define map style.
               // options:['roadmap', 'satellite', 'hybrid', 'terrain','styled_map']
-              mapTypeId: google.maps.MapTypeId.mType,
+              mapTypeId: google.maps.MapTypeId.roadmap,
 
-              mapTypeControlStyle: google.maps.MapTypeControlStyle.cStyle,
+              mapTypeControlStyle: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
 
               // centers the map around specific coordinates
               center: {lat: lt, lng: ln}
             };
   };
 
-////////////////////////////////////////
 
-
-// renderDOMmaps(map_id, m_obj, m_opts) {
-  // var slide_map = document.getElementsByClassName("map")[0];
-
-//   if(parseInt(map_id) == 0) {
-//     return m_obj = new google.maps.Map(slide_map, m_opts);
-//   }
-
-// }
-
-
-// TODO: REFACTOR 4 CONDITIONAL STATEMENTS
-function renderDOMmaps(map_id) {
+function renderDOMmaps(map_id, map_lat, map_lon) {
   map_id = parseInt(map_id);
   // slide_map isolates the slideshow map's DOM element
   var slide_map = document.getElementsByClassName("map")[0];
 
   // instantiates new Google Maps with pre-defined options and
   // places maps into specific DOM elements
-  if(map_id == 0) {
-    return map_1821 = new google.maps.Map(slide_map,
-      setMapOptions(15, "roadmap", "HORIZONTAL_BAR", 44.865782, -91.922486));
-  }
-
-  if(map_id == 1) {
-    return map_1818 = new google.maps.Map(slide_map,
-      setMapOptions(15, "roadmap", "HORIZONTAL_BAR", 44.865979, -91.9252027));
-  }
-
-  if(map_id == 2) {
-    return map_1815 = new google.maps.Map(slide_map,
-      setMapOptions(15, "roadmap", "HORIZONTAL_BAR", 44.866102, -91.9246957));
-  }
-
-  if(map_id == 3) {
-    return map_N4923 = new google.maps.Map(slide_map,
-      setMapOptions(15, "roadmap", "HORIZONTAL_BAR", 44.8589455, -91.8753319));
-  }
-
+    return new google.maps.Map(slide_map,
+      setMapOptions(map_lat, map_lon));
 };
 
-renderDOMmaps(property_id);
+// setMarkerDesc() creates text content within an info popup
+// setMarkerDesc(t *string, d *string)
+function setMarkerDesc(t, d) {
+  return "<h4>" + t + "</h4>" + "<p>" + d + "</p>";
+}
 
-////////////////////////////////////////
+// createsMarker() creates custom map markers using latitude and
+// longitude *float coordinates. The marker is placed on a map
+// *object and a title *string is added for an info popup
+// createMarker(lt *float, ln *float, mp *object, ti *string)
+function createMarker(lt, ln, mp, ti) {
+  return new google.maps.Marker({
+    position: { lat: lt, lng: ln},
+    map: mp,
+    title: ti
+  });
+};
 
+// setMarkerListener() creates a listener for a click event
+// on a marker for a specific map. When the marker is clicked
+// the popup is opened
+// setMarkerListener(marker *object, popup *object, map *object)
+function setMarkerListener(marker, popup, map) {
+  google.maps.event.addListener(marker, "click", function() {
+    popup.open(map, marker);
+  });
+};
 
-// TODO: REMOVE INFO POPUP VARIABLES IN FAVOR OF FUNCTIONS
-
-  // setMarkers creates new map markers, places the markers on each map
+  // placeMarkers creates new map markers, places the markers on each map
   // and creates event listeners for click events on each marker
-  function setMarkers() {
+  function placeMarkers(p_id) {
 
-      ////////////////////////////////////////
-
-      // setMarkerDesc() creates text content within an info popup
-      // setMarkerDesc(t *string, d *string)
-      function setMarkerDesc(t, d) {
-        return "<h4>" + t + "</h4>" + "<p>" + d + "</p>";
-      }
-
+    // TODO: improve commenting
+    // generates a marker
+    // creates click event listeners for each marker
       // infoPopup holds a Google Maps InfoWindow object
       // created from content *strings passed to setMarkerDesc(t, d).
       // the popups open when clicking a Google Map marker
       // new google.maps.InfoWindow creates a Google Maps
-      var map_1821_infoPopup = new google.maps.InfoWindow({
-        content: setMarkerDesc("1821", "Short Property Description")
-      });
-
-      var map_1818_infoPopup = new google.maps.InfoWindow({
-        content: setMarkerDesc("1818", "Short Property Description")}
-      );
-
-      var map_1815_infoPopup = new google.maps.InfoWindow({
-        content: setMarkerDesc("1815", "Short Property Description")}
-      );
-
-      var map_N4923_infoPopup = new google.maps.InfoWindow({
-        content: setMarkerDesc("N4923", "Short Property Description")}
-      );
-
-      ////////////////////////////////////////
-
-
-    // createsMarker() creates custom map markers using latitude and
-    // longitude *float coordinates. The marker is placed on a map
-    // *object and a title *string is added for an info popup
-    // createMarker(lt *float, ln *float, mp *object, ti *string)
-    function createMarker(lt, ln, mp, ti) {
-      return new google.maps.Marker({
-        position: { lat: lt, lng: ln},
-        map: mp,
-        title: ti
-      });
-    };
-
-    var map_1821_marker = createMarker(44.865782, -91.922486, map_1821, "1821 7th Street");
-    var map_1818_marker = createMarker(44.865979, -91.9252027, map_1818, "1818 6th Street");
-    var map_1815_marker = createMarker(44.866102, -91.9246957, map_1815, "1815 7th Street");
-    var map_N4923_marker = createMarker(44.8589455, -91.8753319, map_N4923, "N4923 - 572nd Street");
-
-    // setMarkerListener() creates a listener for a click event
-    // on a marker for a specific map. When the marker is clicked
-    // the popup is opened
-    // setMarkerListener(marker *object, popup *object, map *object)
-    function setMarkerListener(marker, popup, map) {
-      google.maps.event.addListener(marker, "click", function() {
-        popup.open(map, marker);
-      });
-    };
-
-    // creates click event listeners for each marker
-    setMarkerListener(map_1821_marker, map_1821_infoPopup, map_1821);
-    setMarkerListener(map_1818_marker, map_1818_infoPopup, map_1818);
-    setMarkerListener(map_1815_marker, map_1815_infoPopup, map_1815);
-    setMarkerListener(map_N4923_marker, map_N4923_infoPopup, map_N4923);
+      return setMarkerListener(
+        createMarker(app_rental_props[p_id].lat,
+          app_rental_props[p_id].lon,
+          renderDOMmaps(p_id, app_rental_props[p_id].lat, app_rental_props[p_id].lon), app_rental_props[p_id].address),
+        new google.maps.InfoWindow({
+          content: setMarkerDesc(app_rental_props[p_id].address,
+            app_rental_props[p_id].desc)
+        }),
+        app_rental_props[p_id].map);
 
   }
   ////////////////////////////////////////
 
-
-  setMarkers();
-
+  placeMarkers(property_id);
 }
