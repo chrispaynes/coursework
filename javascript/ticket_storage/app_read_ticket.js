0613a4cdc6ac585adf37158f4ec587c33378e243
chrispaynes <cdpaynes@gmail.com>
2016-10-02 01:18:45 -0500

    Improves comments and refactors main to pass one ticket to writeTicket instead of all tickets

--- javascript/ticket_storage/app_read_ticket.js
+++ javascript/ticket_storage/app_read_ticket.js
@@ -1,47 +1,58 @@
 // uses foreign key to read a ticket across parallel arrays
 function readTicket() {
 
-  function getQuery(message){
-    // prompts user to enter a search value
-    // continues with search if user enters a valid 6 digit entry
-    // returns the parsed 6-digit value
-    var query = prompt(message);
-    return validateQuery(query);
+  // getQuery prompts the user with a message to input a query value.
+  // validation_func validates the query value.
+  // getQuery(message *string) => *int
+  function getQuery(message, validation_func){
+    return validation_func(prompt(message));
   }
 
+  // validateQuery validates a user query to ensure the query
+  // exists can be retrieved from the database.
+  // validateQuery(query *string) => *int // *getQuery() // *false
   function validateQuery(query) {
-    // "No records found matching that Ticket Id");
-    var rangeError = "No records found matching that Ticket Id \nSearch values must be between 100000 and 999999";
+    var range_error = "No records found matching that Ticket Id.\nSearch values must be between 100000 and 999999.";
     var reQuery = "Enter a 6-Digit Ticket ID to Begin Your Search";
 
     if(!query || query == "") {
       return;
     }
 
-// getForeignKey(query)
-    if(isValidNumber(Number(query), 100000, 999999) && getForeignKey(query) > -1) {
+    if(isValidNumber(Number(query), 100000, 999999)) {
       return Number(query);
     } else {
-      alert(rangeError);
-      return getQuery(reQuery);
+      alert(range_error);
+      return getQuery(reQuery, validateQuery);
     }
   }
 
-  // displays ticket if found in the database
-  // otherwise prompts user to start a new query
-  function writeTicketToDOM() {
-    // t_index stores a ticket's index value from the database.
-    var t_index = getForeignKey(getQuery("Enter a 6-Digit Ticket ID to Begin Your Search"))
-    initTable()
-    record.children[0].children[0].id = "deleteTicketButton" +ticket_db[t_index].id;
+  // writeTicket returns a ticket object's properties to the DOM.
+  // writeTicket(ticket *obj)
+  function writeTicket(ticket) {
+    console.log("ticket\t", ticket, typeof(ticket));
+    record.children[0].children[0].id = "deleteTicketButton" + ticket.id;
     record.children[0].children[0].innerHTML = "<i class='fa fa-trash-o fa-fw'></i>";
-    record.children[1].textContent = ticket_db[t_index].id;
-    record.children[2].textContent = ticket_db[t_index].client;
-    record.children[3].textContent = ticket_db[t_index].status;
-    record.children[4].innerHTML = "&#36;" + ticket_db[t_index].cost.toLocaleString();
+    record.children[1].textContent = ticket.id;
+    record.children[2].textContent = ticket.client;
+    record.children[3].textContent = ticket.status;
+    record.children[4].innerHTML = "&#36;" + ticket.cost.toLocaleString();
     tbl.appendChild(record.cloneNode(true));
   };
 
-  writeTicketToDOM();
+  function main() {
+    var search = getQuery("Enter a 6-Digit Ticket ID to Begin Your Search", validateQuery);
+    var ticket_index = getForeignKey(search);
 
+    // If the ticket exists in the DB, pass it to writeTicket(),
+    // else leave the DOM as is.
+    if(ticket_index != -1) {
+      initTable();
+      return writeTicket(ticket_db[ticket_index]);
+    } else {
+      return;
+    }
+  }
+
+  main();
 }
