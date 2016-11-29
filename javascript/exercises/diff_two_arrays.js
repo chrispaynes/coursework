3b07106e13cd89afcb7ad019eb340c26205e9a03
chrispaynes <cdpaynes@gmail.com>
2016-11-28 20:24:48 -0600

    Removes loop in favor of filter function

--- javascript/exercises/diff_two_arrays.js
+++ javascript/exercises/diff_two_arrays.js
@@ -1,19 +1,16 @@
 function diffArray(arr1, arr2) {
-  var diffElements = []; //empty array to hold diff values
-  var jointArr = arr1.concat(arr2).sort(ascSort);
+  var concatArray = arr1.concat(arr2).sort(ascSort);
 
-  function ascSort(a, b) {
-    return a - b;
-  }
-
-  //loops through the concatenated arr values
-  //if value is not present in arr1 or arr2, it is pushed to diffElements
-  for (var i in jointArr) {
-    if (arr1.indexOf(jointArr[i]) === -1 || arr2.indexOf(jointArr[i]) === -1) {
-      diffElements.push(jointArr[i]);
+  return concatArray.filter(function(element) {
+    if (arr1.indexOf(element) === -1 || arr2.indexOf(element) === -1) {
+      return element;
     }
-  }
-  return diffElements;
+  });
+
+}
+
+function ascSort(a, b) {
+  return a - b;
 }
 
 module.exports = diffArray;
