b2e75ce082f5a3baa489e46f0c5ba55cfe69edd3
chrispaynes <cdpaynes@gmail.com>
2016-12-14 21:15:56 -0600

    Reformats for better readbility

--- javascript/exercises/spinal_tap_case.js
+++ javascript/exercises/spinal_tap_case.js
@@ -1,9 +1,11 @@
-function capitalize(match, p1, offset, string) {
-  return " " + match.toLowerCase();
-}
-
 function spinalCase(str) {
-  return str.replace(/[A-Z]/g, capitalize).replace(/\s\s/, " ").replace(/^\s/, "").replace(/([\s,\_])/gm, '-').replace(/\-\-/g, "-");
+  let lower = (regex_match) => ' ' + regex_match.toLowerCase()
+
+  return str.replace(/[A-Z]/g, lower)
+    .replace(/^\s/, '')
+    .replace(/\s{2}/, ' ')
+    .replace(/([\s,\_])/gm, '-')
+    .replace(/\-{2}/g, '-');
 }
 
 module.exports = spinalCase;
