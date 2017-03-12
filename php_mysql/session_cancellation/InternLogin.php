7454ee124449f0e2fddca6240dfbe977aca15860
chrispaynes <cdpaynes@gmail.com>
2017-03-11 19:03:55 -0600

    Adds Strict XHTML Doctype

--- php_mysql/session_cancellation/InternLogin.php
+++ php_mysql/session_cancellation/InternLogin.php
@@ -1,9 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta content="text/html charset=UTF-8" http-equiv="Content-Type">
-  <title></title>
-</head>
+<?php
+session_start();
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+
+  <head>
+    <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+    <title></title>
+  </head>
 <body>
 
   <h1>College Internships</h1>
@@ -13,7 +19,7 @@
 
   <!-- Form to allow new interns to register for an intern ID -->
   <h3>New Intern Registration</h3>
-  <form action="RegisterIntern.php" method="post">
+  <form action="RegisterIntern.php?<?php echo SID; ?>" method="post">
     <p>Enter your name: First <input name="first" type="text"> Last: <input name="last" type="text"></p>
     <p>Enter your e-mail address: <input name="email" type="text"></p>
     <p>Enter a password for your account: <input name="password" type="password"></p>
@@ -24,7 +30,7 @@
 
   <!-- Allow returning users to log in -->
   <h3>Returning Intern Login</h3>
-  <form action="VerifyLogin.php" method="post">
+  <form action="VerifyLogin.php?<?php echo SID; ?>" method="post">
     <p>Enter your e-mail address: <input name="email" type="text"></p>
     <p>Enter your password: <input name="password" type="password"></p>
     <p><em>(Passwords are case-sensitive and must be a least 6 characters long)</em></p><input name="reset" type="reset" value="Reset Login Form"> <input name="login" type="submit" value="Log In">
