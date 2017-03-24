1d76e17c9fd757f430390fc3016cb6280cc4dfe7
chrispaynes <chris@Chriss-MacBook-Pro.local>
2017-03-24 13:22:37 -0500

    Enables bot to respond with simple  message

--- javascript/node_slackbot/bin/run.js
+++ javascript/node_slackbot/bin/run.js
@@ -8,10 +8,12 @@ const botToken = process.env.SLACK_BOT_TOKEN || '';
 const logLevel = "verbose";
 const rtm = slackClient.init(botToken, logLevel);
 
-// Starts the Real-Time Messaging (RTM) service
+// Starts the Real-Time Messaging (RTM) service.
 rtm.start();
 
-server.listen(3000);
+// slackClient.addAuthenticatedHandler starts the express server upon rtm authentication. 
+slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
+
 server.on("listening", () => {
     console.log(`IRIS is listening on ${server.address().port} in ${service.get("env")} mode.`)
 });
--- javascript/node_slackbot/server/slackClient.js
+++ javascript/node_slackbot/server/slackClient.js
@@ -2,20 +2,36 @@
 
 require("../env");
 const RtmClient = require('@slack/client').RtmClient;
-const CLIENT = require('@slack/client').CLIENT_EVENTS;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+let rtm = null;
 
 // handleOnAuthenticated logs initial Real-Time Messaging (RTM) starting data.
 function handleOnAuthenticated(rtmStartData) {
     console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name} but not yet connected to a channel`);
 }
 
-// addAuthenticatedHandler adds an event handler function when the client emits an RTM.AUTHENTICATED event.
-function addAuthenticatedHandler(rtm, handler) {
-    rtm.on(CLIENT.RTM.AUTHENTICATED, handler)
+// handleOnMessage handles incoming messages.
+function handleOnMessage(message) {
+    console.log(message);
+
+    rtm.sendMessage("Hello!", message.channel);
+
 }
 
+// addAuthenticatedHandler adds an event handler function
+// when the client emits an RTM.AUTHENTICATED event.
+function addAuthenticatedHandler(rtm, handler) {
+    rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler)
+}
+
+// init initializes and returns a new slack client with
+// authentication event handling and message handling.
 module.exports.init = (botToken, logLevel) => {
-    const rtm = new RtmClient(botToken, { logLevel: logLevel });
+    rtm = new RtmClient(botToken, { logLevel: logLevel });
     addAuthenticatedHandler(rtm, handleOnAuthenticated);
+    rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
     return rtm;
 };
+
+module.exports.addAuthenticatedHandler = addAuthenticatedHandler;
