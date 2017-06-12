2b2e6bf1cdc50d66612ca3eed7de8e7b92cdf1a8
chrispaynes <cdpaynes@gmail.com>
2017-06-11 23:58:33 -0500

    Adds cookies to presentation logic

--- javascript/VueForum/src/presentation/components/Header.js
+++ javascript/VueForum/src/presentation/components/Header.js
@@ -7,16 +7,45 @@ Vue.component('app-header', {
             </div>
             <div id='nav_component' class='col-xs-12 col-md-8 text-size-sm'>
                 <div class='container-fluid'>
-                    <div class='row'>
+                    <div class='row' v-if="isLoggedOut">
                         <router-link class='col-xs-12 col-sm-3' to='/'>Home</router-link>
+                        <router-link class='col-xs-12 col-sm-3' to='/profile'>View Users</router-link>
                         <router-link class='col-xs-12 col-sm-3' to='/register'>Register</router-link>
-                        <router-link class='col-xs-12 col-sm-3' to='/profile'>User Profile</router-link>
-                        <router-link class='col-xs-12 col-sm-3' to='/thread/new'>New Thread</router-link>
+                        <router-link class='col-xs-12 col-sm-3' to='/register'>Login</router-link>
+                    </div>
+                    <div class='row' v-else>
+                        <router-link class='col-xs-12 col-sm-2' to='/'>Home</router-link>
+                        <router-link class='col-xs-12 col-sm-2' to='/profile'>User Profile</router-link>
+                        <router-link class='col-xs-12 col-sm-2' to='/profile'>View Users</router-link>
+                        <router-link class='col-xs-12 col-sm-2' to='/thread/new'>New Thread</router-link>
+                        <a href='#' class='col-xs-12 col-sm-4' v-on:click="logout()">{{this.username}} (Logout)</a>
                     </div>
                 </div>
             </div>
         </div>
+
         <hr class='hr-bdr-2 margin-0' />
     </div>
     `,
+    data: function() {
+        return { username: this.username, user_id: this.username, isLoggedOut: this.isLoggedOut };
+    },
+    beforeCreate: function() {
+        this.isLoggedOut = true;
+
+        if(Cookies.length > 0 && Cookies.get('username') && Cookies.get('user_id')) {
+            this.username = Cookies.get('username');
+            this.user_id = Cookies.get('user_id');
+            this.isLoggedOut = false;
+        }
+    },
+    methods: {
+        logout: function() {
+            this.username = '';
+            this.user_id = '';
+            this.isLoggedOut = true;
+            Cookies.remove('username');
+            Cookies.remove('user_id');
+        },
+    },
 });
--- javascript/VueForum/src/presentation/components/New-Thread.js
+++ javascript/VueForum/src/presentation/components/New-Thread.js
@@ -5,16 +5,30 @@ var NewThread = Vue.component('new-thread-form', {
             <h1 class='text-center margin-bottom-lg'>Start A New Thread</h1>
                 <div class='form-group col-sm-12'>
                     <label class='col-xs-12 col-sm-2 txt-green'>Thread Title</label>
-                    <input class='col-xs-12 col-sm-10' type='text' name='lname' placeholder='' required></textarea>
+                    <input class='col-xs-12 col-sm-10' type='text' name='title' placeholder='' required></textarea>
                 </div>
                 <div class='form-group col-sm-12'>
                     <label class='col-xs-12 col-sm-2 txt-green'>Thread Message</label>
-                    <textarea rows='12' class='col-xs-12 col-sm-10' type='text' name='lname' placeholder='' required></textarea>
+                    <textarea rows='12' class='col-xs-12 col-sm-10' type='text' name='body' placeholder='' required></textarea>
                 </div>
+                <input class='hidden' type='text' name='username' v-bind:value='this.username' required></textarea>
+                <input class='hidden' type='text' name='user_id' v-bind:value='this.user_id' required></textarea>
+                <input class='hidden' type='text' name='timestamp' v-bind:value='new Date()' required></textarea>
                 <div class='form-group col-sm-12 text-center'>
-                    <input class='btn btn-success' type='submit' name='register' value='Submit Thread'>
+                    <input class='btn btn-success' type='submit' name='new_thread' value='Submit Thread'>
                 </div>
             </form>
         </div>
     `,
+    data: function() {
+        return { username: this.username, user_id: this.user_id };
+    },
+    beforeCreate: function() {
+        var self = this;
+
+        if(Cookies.length > 0 && Cookies.get('username') && Cookies.get('user_id')) {
+            self.username = Cookies.get('username');
+            self.user_id = Cookies.get('user_id');
+        }
+    },
 });
